514fee8ba71927a1aedff84268baae38674ef896
manfred-commits <manfredipiraino@gmail.com>
2022-02-09 12:14:35 +0100

    add: myExe Exceptions v2

--- src/MyEXE_Exception.java
+++ src/MyEXE_Exception.java
@@ -1,24 +1,37 @@
 
+
+
 public class MyEXE_Exception {
 
 	public static void main(String[] args) {
 		
-		String myStringArray="a";
-		Integer myDivisor=5;
-		Integer myConvertedValue=myConvertToInteger(myStringArray);
-		System.out.println("Il valore convertito è pari a: "+myConvertedValue+" il divisore è: "+myDivisor);
-		System.out.println("Il risultato è: "+myDivision(myConvertedValue,myDivisor));
+		String myStringArray="3";
+		String [] stringArray={"a","2","3"};
+		Integer myDivisor=3;
+		int [] myConvertedValue=myConvertToInteger(stringArray);
+		int counter=0;
+		for(int singleConversion:myConvertedValue) {
+			System.out.println("Il valore convertito per '"+stringArray[counter]+"' è pari a: "+singleConversion+" il dividendo è: "+myDivisor);
+			System.out.println("Divisione: "+myDivision(singleConversion,myDivisor));
+			counter++;
+		}
+		
+		
 	}
 
-	public static int myConvertToInteger(String myString) {
-		int myConvertedString = 0;
-		try {
-			myConvertedString = Integer.parseInt(myString);
-			return myConvertedString;
-		} catch (NumberFormatException e) {
-			System.out.println("Impossibile convertire in un numero intero - " + e.getMessage());
-			return myConvertedString;
+	public static int[] myConvertToInteger(String [] myString) {
+		int[] myConvertedString =new int[myString.length];
+		int counter=0;
+		for(String singleConversion:myString) {
+			try {
+				myConvertedString[counter] = Integer.parseInt(singleConversion);
+			} catch (NumberFormatException e) {
+				System.out.println("Impossibile convertire in un numero intero - " + e.getMessage());
+				myConvertedString[counter] =0;
+			}
+			counter++;
 		}
+		return myConvertedString;		
 		
 	}
 
